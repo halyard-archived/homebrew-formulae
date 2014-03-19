1870c2263140d2a086aec3a473175ebb011f8a1c
Les Aker <me@lesaker.org>
2014-03-19 19:08:10 -0400

    add flint

---
+++ flint.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Flint < Formula
+  homepage 'https://github.com/pengwynn/flint'
+  url 'https://github.com/pengwynn/flint/releases/download/v0.0.3/flint_darwin_amd64'
+  sha256 '349e1289bb51488199acd5ba1c6ff9960d05fe10f2e3f64253b534bf55c7fd64'
+  version '0.0.3'
+
+  def install
+    system 'mv flint_darwin_amd64 flint'
+    bin.install 'flint'
+  end
+end
