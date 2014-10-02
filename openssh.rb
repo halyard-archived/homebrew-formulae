fae357e2d55128df3582bf3b600de8629bd946f1
Les Aker <me@lesaker.org>
2014-10-01 21:03:28 -0400

    openssh was meant to be patched

--- openssh.rb
+++ openssh.rb
@@ -6,27 +6,22 @@ class Openssh < Formula
   version '6.6p1'
   sha256 '48c1f0664b4534875038004cc4f3555b8329c2a81c1df48db5c517800de203bb'
 
-  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
-  option 'with-keychain-support', 'Add native OS X Keychain and Launch Daemon support to ssh-agent'
-
-  depends_on 'autoconf' => :build if build.with? 'keychain-support'
-  depends_on 'openssl' if build.with? 'brewed-openssl'
+  depends_on 'autoconf' => :build
+  depends_on 'openssl'
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
   def patches
     p = []
-    p << 'https://trac.macports.org/export/117984/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch' if build.include? 'with-keychain-support'
+    p << 'https://trac.macports.org/export/117984/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch'
     p
   end
 
   def install
-    system "autoreconf -i" if build.with? 'keychain-support'
+    system "autoreconf -i"
 
-    if build.include? "with-keychain-support"
-        ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
-        ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
-    end
+    ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+    ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
 
     args = %W[
       --with-libedit
@@ -35,13 +30,13 @@ class Openssh < Formula
       --sysconfdir=#{etc}/ssh
     ]
 
-    args << "--with-ssl-dir=#{Formula["openssl"].opt_prefix}" if build.with? 'brewed-openssl'
+    args << "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
     args << "--with-ldns" if build.with? "ldns"
 
     # Sometimes when Apple ships security update, the libraries get
     # updated while the headers don't. Disable header/library version
     # check when using system openssl to cope with this situation.
-    args << "--without-openssl-header-check" if not build.with? 'brewed-openssl'
+    args << "--without-openssl-header-check"
 
     system "./configure", *args
     system "make"
@@ -49,19 +44,17 @@ class Openssh < Formula
   end
 
   def caveats
-    if build.include? "with-keychain-support"
-      <<-EOS.undent
-        For complete functionality, please modify:
-          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
+    <<-EOS.undent
+      For complete functionality, please modify:
+        /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
 
-        and change ProgramArugments from
-          /usr/bin/ssh-agent
-        to
-          #{HOMEBREW_PREFIX}/bin/ssh-agent
+      and change ProgramArugments from
+        /usr/bin/ssh-agent
+      to
+        #{HOMEBREW_PREFIX}/bin/ssh-agent
 
-        After that, you can start storing private key passwords in
-        your OS X Keychain.
-      EOS
-    end
+      After that, you can start storing private key passwords in
+      your OS X Keychain.
+    EOS
   end
 end
