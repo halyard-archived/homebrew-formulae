e9e3324917ea3f46ac304324aa115243b3afd7c1
Les Aker <me@lesaker.org>
2014-10-07 08:09:17 -0700

    use dynamic URL for flint

--- flint.rb
+++ flint.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Flint < Formula
   homepage 'https://github.com/pengwynn/flint'
-  url 'https://github.com/pengwynn/flint/releases/download/v0.0.3/flint_darwin_amd64'
+  url "https://github.com/pengwynn/flint/releases/download/v#{version}/flint_darwin_amd64"
   sha256 '349e1289bb51488199acd5ba1c6ff9960d05fe10f2e3f64253b534bf55c7fd64'
   version '0.0.3'
 
