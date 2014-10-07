2ab4d4b79d0d3f4757ea7ee6b63338343246139b
Les Aker <me@lesaker.org>
2014-10-07 08:18:21 -0700

    update openssh to 6.7p1

--- openssh.rb
+++ openssh.rb
@@ -2,19 +2,18 @@ require 'formula'
 
 class Openssh < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.6p1.tar.gz'
-  version '6.6p1'
-  sha256 '48c1f0664b4534875038004cc4f3555b8329c2a81c1df48db5c517800de203bb'
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-#{version}.tar.gz"
+  version '6.7p1'
+  sha256 'b2f8394eae858dabbdef7dac10b99aec00c95462753e80342e530bbb6f725507'
 
   depends_on 'autoconf' => :build
   depends_on 'openssl'
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
-  def patches
-    p = []
-    p << 'https://trac.macports.org/export/117984/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch'
-    p
+  patch do
+    url 'https://trac.macports.org/export/117984/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch'
+    sha256 'd12bb6daacbea76ccc33ec8edd467946cd1a63d251822c036ed6d098bbee2aee'
   end
 
   def install
@@ -25,12 +24,13 @@ class Openssh < Formula
 
     args = %W[
       --with-libedit
+      --with-pam
       --with-kerberos5
       --prefix=#{prefix}
       --sysconfdir=#{etc}/ssh
+      --with-ssl-dir=#{Formula["openssl"].opt_prefix}
     ]
 
-    args << "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
     args << "--with-ldns" if build.with? "ldns"
 
     # Sometimes when Apple ships security update, the libraries get
