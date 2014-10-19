8c3a2a4f9929a7d1666d62dc4c243d7a207a1011
Les Aker <me@lesaker.org>
2014-10-19 00:12:47 -0400

    rename flint

--- flint-akerl.rb
+++ flint-akerl.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Flint < Formula
+class FlintAkerl < Formula
   homepage 'https://github.com/pengwynn/flint'
   url "https://github.com/pengwynn/flint/releases/download/v#{version}/flint_darwin_amd64"
   sha256 '349e1289bb51488199acd5ba1c6ff9960d05fe10f2e3f64253b534bf55c7fd64'
