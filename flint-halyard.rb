2203280ca55eed4317bc12667420cf83e843075c
Les Aker <me@lesaker.org>
2015-03-08 08:33:45 -0400

    update flint to 0.0.4

--- flint-halyard.rb
+++ flint-halyard.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class FlintHalyard < Formula
   homepage 'https://github.com/pengwynn/flint'
-  version '0.0.3'
-  url "https://github.com/pengwynn/flint/releases/download/v#{version}/flint_darwin_amd64"
-  sha256 '349e1289bb51488199acd5ba1c6ff9960d05fe10f2e3f64253b534bf55c7fd64'
+  version '0.0.4'
+  url "https://github.com/pengwynn/flint/releases/download/v#{version}/flint_darwin_amd64.zip"
+  sha256 'a872d3426bbab7618a25de0793f864fa2050c7de34bdec8a4c8a8967ad8ede6d'
 
   def install
     system 'mv flint_darwin_amd64 flint'
