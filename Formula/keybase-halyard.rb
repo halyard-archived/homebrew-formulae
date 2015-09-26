71bf20b84b8bcd245da6172c2391b7454988fee8
Les Aker <me@lesaker.org>
2015-09-25 20:40:39 -0400

    add keybase

---
+++ Formula/keybase-halyard.rb
@@ -0,0 +1,23 @@
+class KeybaseHalyard < Formula
+  desc 'Keybase'
+  homepage 'https://keybase.io/'
+
+  version '1.0.0-25'
+  url "https://github.com/keybase/client-beta/archive/v#{version}.tar.gz"
+  sha256 '47f29fbce993dcacbfa790476699d8946bded97e2053310744dfff36475f1cba'
+  head 'https://github.com/keybase/client-beta.git'
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    ENV['GOBIN'] = buildpath
+    system 'mkdir', '-p', 'src/github.com/keybase/'
+    system 'mv', 'client', 'src/github.com/keybase/'
+
+    system 'go', 'get', 'github.com/keybase/client/go/keybase'
+    system 'go', 'build', '-a', '-tags', 'release brew', '-o', 'keybase', 'github.com/keybase/client/go/keybase'
+
+    bin.install 'keybase'
+  end
+end
