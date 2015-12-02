cb1132200b8dab388add708e84d04b1f640c8825
Les Aker <me@lesaker.org>
2015-12-02 09:46:09 -0500

    update keybase

--- Formula/keybase-halyard.rb
+++ Formula/keybase-halyard.rb
@@ -2,24 +2,25 @@ class KeybaseHalyard < Formula
   desc 'Keybase'
   homepage 'https://keybase.io/'
 
-  version '1.0.0-25'
-  url "https://github.com/keybase/client-beta/archive/v#{version}.tar.gz"
-  sha256 '47f29fbce993dcacbfa790476699d8946bded97e2053310744dfff36475f1cba'
-  head 'https://github.com/keybase/client-beta.git'
+  version '1.0.5-0'
+  url "https://github.com/keybase/client/archive/v#{version}.tar.gz"
+  sha256 'fcc41dbe3c602c33dd5853fd81ab66baa91c2cc0524ed5bdf89456f2df143e8a'
+  head 'https://github.com/keybase/client.git'
 
   depends_on 'go' => :build
 
   def install
-    inreplace 'client/go/libkb/constants_release.go', '//const', 'const'
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = buildpath
+    ENV["GO15VENDOREXPERIMENT"] = "1"
+    (buildpath/"src/github.com/keybase/client/").install "go"
 
-    ENV['GOPATH'] = buildpath
-    ENV['GOBIN'] = buildpath
-    system 'mkdir', '-p', 'src/github.com/keybase/'
-    system 'mv', 'client', 'src/github.com/keybase/'
+    system "go", "build", "-a", "-tags", "production brew", "github.com/keybase/client/go/keybase"
 
-    system 'go', 'get', 'github.com/keybase/client/go/keybase'
-    system 'go', 'build', '-a', '-tags', 'release brew', '-o', 'keybase', 'github.com/keybase/client/go/keybase'
+    bin.install "keybase"
+  end
 
-    bin.install 'keybase'
+  test do
+    system "#{bin}/keybase", "-standalone", "id", "homebrew"
   end
 end
