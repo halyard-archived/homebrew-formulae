af6cb35a8698e15fa03366db274474f8e1c09e32
Les Aker <me@lesaker.org>
2015-12-02 11:26:09 -0500

    upgrade pick-halyard to 1.2.1

--- Formula/pick-halyard.rb
+++ Formula/pick-halyard.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class PickHalyard < Formula
   homepage 'https://github.com/thoughtbot/pick'
-  version '1.2.0'
+  version '1.2.1'
   url "https://github.com/thoughtbot/pick/releases/download/v#{version}/pick-#{version}.tar.gz"
-  sha256 'c7593f5dde7d13177f1258b941d635761b4ea38e4c991aa7916ce4e3f581e090'
+  sha256 '048fe3733460cc284ddda1edada80b819b66f331bc0a88f489637b9e3a500700'
 
   def install
     system './configure', "--prefix=#{prefix}"
