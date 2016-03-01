555b0a053df910f3dedd31f185f346e5ecb41ac0
Les Aker <me@lesaker.org>
2016-03-01 10:45:53 -0500

    update rclone

--- Formula/rclone-halyard.rb
+++ Formula/rclone-halyard.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RcloneHalyard < Formula
   homepage 'http://rclone.org'
-  version '1.27'
+  version '1.28'
   url "http://downloads.rclone.org/rclone-v#{version}-osx-amd64.zip"
-  sha256 '7de9e938496ce4eed5046de864152c3da05e6d11a154a2d6ded4e6ddedc00b2a'
+  sha256 'e8c878ae7e4c36145b6c848b0973baa8da59152195b0670142b4471f5f2614a9'
 
   def install
     bin.install 'rclone'
