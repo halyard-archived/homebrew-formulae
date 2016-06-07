62a5079962181c7d4162d3e3677e7fc1f275d293
Les Aker <me@lesaker.org>
2016-06-06 21:40:54 -0400

    update pick

--- Formula/pick-halyard.rb
+++ Formula/pick-halyard.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class PickHalyard < Formula
   homepage 'https://github.com/thoughtbot/pick'
-  version '1.3.0'
+  version '1.4.0'
   url "https://github.com/thoughtbot/pick/releases/download/v#{version}/pick-#{version}.tar.gz"
-  sha256 'a1c2c7902f6a322d7be5cf024d6cb94f1e65edfbba4b151647d215481f783257'
+  sha256 '46f46b0df54cf27e8dd19ae291d5534cb55ef37d9cdb3cc774cd88c809f718fd'
 
   def install
     system './configure', "--prefix=#{prefix}"
