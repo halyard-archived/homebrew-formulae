0bb1d9a50b3f22459d4ac8de9d7f18c1e2bfad5f
Les Aker <me@lesaker.org>
2016-09-18 15:41:14 -0400

    fix pick cops

--- Formula/pick-halyard.rb
+++ Formula/pick-halyard.rb
@@ -1,17 +1,15 @@
-require "formula"
-
 class PickHalyard < Formula
-  homepage 'https://github.com/thoughtbot/pick'
-  version '1.4.0'
-  url "https://github.com/thoughtbot/pick/releases/download/v#{version}/pick-#{version}.tar.gz"
-  sha256 '46f46b0df54cf27e8dd19ae291d5534cb55ef37d9cdb3cc774cd88c809f718fd'
+  desc "Command line arg picker"
+  homepage "https://github.com/thoughtbot/pick"
+  url "https://github.com/thoughtbot/pick/releases/download/v1.4.0/pick-1.4.0.tar.gz"
+  sha256 "46f46b0df54cf27e8dd19ae291d5534cb55ef37d9cdb3cc774cd88c809f718fd"
 
   def install
-    system './configure', "--prefix=#{prefix}"
-    system 'make', 'install'
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/pick", '-v'
+    system "#{bin}/pick", "-v"
   end
 end
