37af178b3b0d0eca7d11c62f797cfa199c6e638e
Les Aker <me@lesaker.org>
2016-09-18 15:44:44 -0400

    update slacker

--- Formula/slacker-halyard.rb
+++ Formula/slacker-halyard.rb
@@ -1,18 +1,21 @@
 class SlackerHalyard < Formula
-  desc 'MAC to EUI64 converter'
-  homepage 'https://github.com/akerl/slacker'
+  desc "MAC to EUI64 converter"
+  homepage "https://github.com/akerl/slacker"
 
-  version '0.0.2'
-  url "https://github.com/akerl/slacker/archive/#{version}.tar.gz"
-  sha256 '24a375fc25276a9a31ecc33e7f1bc3b093faf7f2e7d539765f5c2fbb80ce0e90'
-  head 'https://github.com/akerl/slacker.git'
+  url "https://github.com/akerl/slacker/archive/0.0.2.tar.gz"
+  sha256 "24a375fc25276a9a31ecc33e7f1bc3b093faf7f2e7d539765f5c2fbb80ce0e90"
+  head "https://github.com/akerl/slacker.git"
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    ENV['GOBIN'] = buildpath
-    system 'go', 'build', '-o', 'slacker'
-    bin.install 'slacker'
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = buildpath
+    system "go", "build", "-o", "slacker"
+    bin.install "slacker"
+  end
+
+  test do
+    system bin/"slacker", "--version"
   end
 end
