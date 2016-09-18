598860d5ca36631ac10d7f0247228390e8bd2162
Les Aker <me@lesaker.org>
2016-09-18 15:46:48 -0400

    fix sparkey cops

--- Formula/sparkey-halyard.rb
+++ Formula/sparkey-halyard.rb
@@ -1,28 +1,26 @@
-require 'formula'
-
 class SparkeyHalyard < Formula
-  version '0.2.0'
-  sha256 'a06caf23c64e7ebae5b8b67272b21ab4c57f21a66d190bfe0a95f5af1dc69154'
+  desc "Storage library"
+  homepage "https://github.com/spotify/sparkey"
 
-  url "https://github.com/spotify/sparkey/archive/sparkey-#{version}.tar.gz"
-  homepage 'https://github.com/spotify/sparkey'
+  url "https://github.com/spotify/sparkey/archive/sparkey-0.2.0.tar.gz"
+  sha256 "a06caf23c64e7ebae5b8b67272b21ab4c57f21a66d190bfe0a95f5af1dc69154"
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'snappy'
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "snappy"
 
   def install
-    system 'autoreconf --install'
-    system "./configure --prefix=#{prefix}"
-    system 'make'
-    system 'make install'
+    system "autoreconf", "--install"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/sparkey createlog -c snappy test.spl"
+    system "#{bin}/sparkey", "createlog", "-c", "snappy", "test.spl"
     system "echo foo.bar | #{bin}/sparkey appendlog -d . test.spl"
-    system "#{bin}/sparkey writehash test.spl"
+    system "#{bin}/sparkey", "writehash", "test.spl"
     system "#{bin}/sparkey get test.spi foo | grep ^bar$"
   end
 end
