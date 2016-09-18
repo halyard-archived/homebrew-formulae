37092542545832386e40f9cef948fc865738f70f
Les Aker <me@lesaker.org>
2016-09-18 15:43:25 -0400

    update rclone for cops

--- Formula/rclone-halyard.rb
+++ Formula/rclone-halyard.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class RcloneHalyard < Formula
-  homepage 'http://rclone.org'
-  version '1.33'
-  url "http://downloads.rclone.org/rclone-v#{version}-osx-amd64.zip"
-  sha256 '71e6051a46cf72715a77f6534a6ca6e1a6dcaf1c33132720e806d84e35f1c60b'
+  desc "File syncing tool"
+  homepage "http://rclone.org"
+  url "http://downloads.rclone.org/rclone-v1.33-osx-amd64.zip"
+  sha256 "71e6051a46cf72715a77f6534a6ca6e1a6dcaf1c33132720e806d84e35f1c60b"
 
   def install
-    bin.install 'rclone'
-    man1.install 'rclone.1'
+    bin.install "rclone"
+    man1.install "rclone.1"
+  end
+
+  test do
+    system bin/"rclone", "--version"
   end
 end
