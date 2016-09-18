6e39e4612c7aa9b1dbee8ff770a2a76edd69503f
Les Aker <me@lesaker.org>
2016-09-18 15:35:52 -0400

    fix krb5 cops

--- Formula/krb5-pkinit.rb
+++ Formula/krb5-pkinit.rb
@@ -4,12 +4,12 @@ class Krb5Pkinit < Formula
   url "http://web.mit.edu/kerberos/dist/krb5/1.14/krb5-1.14.2.tar.gz"
   sha256 "6bcad7e6778d1965e4ce4af21d2efdc15b274c5ce5c69031c58e4c954cda8b27"
 
-  keg_only :provided_by_osx
-
   bottle do
     root_url "https://github.com/halyard/homebrew-formulae/releases/download/krb5-pkinit-1.14.2"
     sha256 "5168c2c12be410fe3c6ced1318086036fb41e2492e6a95e01bea29c6f2a6726c" => :el_capitan
   end
+
+  keg_only :provided_by_osx
 
   depends_on "openssl"
   depends_on "bison" unless OS.mac?
