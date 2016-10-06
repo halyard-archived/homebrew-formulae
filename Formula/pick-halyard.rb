b62467e044dceed6e411a4d4ed47efb2170b443f
Les Aker <me@lesaker.org>
2016-10-05 22:41:53 -0400

    update pick

--- Formula/pick-halyard.rb
+++ Formula/pick-halyard.rb
@@ -1,9 +1,9 @@
 class PickHalyard < Formula
   desc "Command line arg picker"
   homepage "https://github.com/thoughtbot/pick"
-  _version = "1.5.0"
+  _version = "1.5.1"
   url "https://github.com/thoughtbot/pick/releases/download/v#{_version}/pick-#{_version}.tar.gz"
-  sha256 "1dac1c9cf0b14b0bbb4d9eefb6af72cdf5314af7b2ff9e4c7c8858014ad63549"
+  sha256 "d41db6c0f1a87e8463c01a15670a96ec90d978dc83dcec7619a46dfe52acbe26"
 
   def install
     system "./configure", "--prefix=#{prefix}"
