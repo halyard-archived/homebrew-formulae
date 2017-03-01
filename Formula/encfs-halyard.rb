3e17add2f33fcfd3ef2e16ea8dde3b29e280993a
Les Aker <me@lesaker.org>
2017-02-28 22:45:11 -0500

    fix formula include path

--- Formula/encfs-halyard.rb
+++ Formula/encfs-halyard.rb
@@ -5,35 +5,27 @@ class EncfsHalyard < Formula
   sha256 "67203aeff7a06ce7be83df4948db296be89a00cffe1108a0a41c96d7481106a4"
   head "https://github.com/vgough/encfs.git"
 
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "intltool" => :build
-  depends_on "gettext" => :build
   depends_on "boost"
-  depends_on "rlog"
+  depends_on "gettext"
   depends_on "openssl"
-  depends_on "xz"
+  depends_on :osxfuse
 
   needs :cxx11
 
   def install
     ENV.cxx11
-    system "make", "-f", "Makefile.dist"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-boost=#{HOMEBREW_PREFIX}"
-    system "make"
-    system "make", "install"
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, '-DCMAKE_INCLUDE_PATH=/usr/local/include', '-DCMAKE_LIBRARY_PATH=/usr/local/lib'
+      system "make", "install"
+    end
   end
 
   test do
-    if Pathname.new("/Library/Filesystems/osxfusefs.fs/Support/load_osxfusefs").exist?
-      (testpath/"print-password").write("#!/bin/sh\necho password")
-      chmod 0755, testpath/"print-password"
-      system "yes | #{bin}/encfs --standard --extpass=#{testpath}/print-password #{testpath}/a #{testpath}/b"
-      system "umount", testpath/"b"
-    end
+    # Functional test violates sandboxing, so punt.
+    # Issue #50602; upstream issue vgough/encfs#151
+    assert_match version.to_s, shell_output("#{bin}/encfs 2>&1", 1)
   end
 end
