843951993432b8c26454eb738996b68da46e1dd4
Les Aker <me@lesaker.org>
2017-05-13 16:47:35 -0400

    update terraform

--- Formula/terraform-halyard.rb
+++ Formula/terraform-halyard.rb
@@ -1,9 +1,9 @@
 class TerraformHalyard < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  version "0.9.4"
+  version "0.9.5"
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
-  sha256 "73ec3c66a77e0c0879e6397fe2b4c4910b24464971fd0c27795b0fa09143f9ad"
+  sha256 "83b5596c2a510925f90a6572d237b864bc4cf277609ebac294c8f400261e657c"
 
   def install
     bin.install 'terraform'
