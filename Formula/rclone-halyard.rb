625dbe72525c41b8cbbfa6d8525751ec91dfd4a9
Les Aker <me@lesaker.org>
2017-07-24 07:33:10 -0400

    update rclone

--- Formula/rclone-halyard.rb
+++ Formula/rclone-halyard.rb
@@ -1,9 +1,9 @@
 class RcloneHalyard < Formula
   desc "File syncing tool"
   homepage "http://rclone.org"
-  version "1.36"
+  version "1.37"
   url "http://downloads.rclone.org/rclone-v#{version}-osx-amd64.zip"
-  sha256 "3ec7d5658258ad99480ed7b07d552fe4d5d01677fe765f890e0cc1ed08c82b26"
+  sha256 "0eb39c4774a3c90f21ba5ac67bc2684e0ac8cf1a625ae4a92d981a2c842f8d99"
 
   def install
     bin.install "rclone"
