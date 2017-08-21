200353e40eac0d3b3dcce00eb3d0428ebce453e0
Les Aker <me@lesaker.org>
2017-08-21 11:06:51 -0400

    update speculate

--- Formula/speculate-halyard.rb
+++ Formula/speculate-halyard.rb
@@ -2,9 +2,9 @@ class SpeculateHalyard < Formula
   desc "Tool for assuming roles in AWS accounts"
   homepage "https://github.com/akerl/speculate"
 
-  version "0.0.5"
-  url "https://github.com/akerl/speculate/releases/download/v#{version}/speculate_darwin"
-  sha256 "a85e95c4c5b9a1fc832166abd60682d153d573069268331ed0da0cadbf968e17"
+  version "0.0.10"
+  url "https://github.com/akerl/speculate/releases/download/#{version}/speculate_darwin"
+  sha256 "99776a481ef5e442c9c579b0128cf5f75fb6b577e97abc49e3420e1f586fae0c"
 
   head do
     url "https://github.com/akerl/speculate.git"
