d7b67c45e8b8b2488aaf4445e6e95e23950c72f0
Les Aker <me@lesaker.org>
2018-01-06 11:21:18 -0500

    update etcher

--- Formula/etcher-halyard.rb
+++ Formula/etcher-halyard.rb
@@ -1,9 +1,9 @@
 class EtcherHalyard < Formula
   desc "Etcher"
   homepage "https://etcher.io/cli/"
-  version "1.2.1"
+  version "1.3.0"
   url "https://github.com/resin-io/etcher/releases/download/v#{version}/Etcher-cli-#{version}-darwin-x64.tar.gz"
-  sha256 "18c20ace231bdb0b954d89946a98de25c869c918e7399e7fb7133486dd0eff55"
+  sha256 "07ec2c25070a1c9f32eeb95e478c35bc27c7ed7e728cada25e92113baaa17709"
 
   def install
     bin.install "etcher"
