26a7b24f36487e486a4b5867372cabcead2826df
Les Aker <me@lesaker.org>
2018-02-04 07:53:11 -0500

    update keybase

--- Formula/keybase-halyard.rb
+++ Formula/keybase-halyard.rb
@@ -2,8 +2,8 @@ class KeybaseHalyard < Formula
   desc "Keybase"
   homepage "https://keybase.io/"
 
-  url "https://github.com/keybase/client/archive/v1.0.39.tar.gz"
-  sha256 "f6835f7cab68eb198aaae6df5584343b33249b69603c9181d2112bb0778908f9"
+  url "https://github.com/keybase/client/archive/v1.0.40.tar.gz"
+  sha256 "79f11737b3bdc279b34d2b978584b3c12e1c24ccdeb3b86dbf7670aa9c634913"
   head "https://github.com/keybase/client.git"
 
   depends_on "go" => :build
