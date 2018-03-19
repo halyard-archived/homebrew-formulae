4613fbfd6ce351bbbe69badadfd0168905a2369c
Les Aker <me@lesaker.org>
2018-03-19 10:57:52 -0400

    update rclone

--- Formula/rclone-halyard.rb
+++ Formula/rclone-halyard.rb
@@ -1,16 +1,27 @@
 class RcloneHalyard < Formula
-  desc "File syncing tool"
-  homepage "http://rclone.org"
-  version "1.39"
-  url "http://downloads.rclone.org/rclone-v#{version}-osx-amd64.zip"
-  sha256 "58580f32325bc373687f87d8ba91c39ca12de3ce409fbfb32cf6a6173cd8c0e3"
+  desc "Rsync for cloud storage"
+  homepage "https://rclone.org/"
+  url "https://github.com/ncw/rclone/archive/v1.40.tar.gz"
+  sha256 "9faead37d01681ea10cf1d3c9a0d2db241fa858212f3d0aa0325096168a97abd"
+  head "https://github.com/ncw/rclone.git"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "rclone"
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/ncw/"
+    ln_s buildpath, buildpath/"src/github.com/ncw/rclone"
+    system "go", "build", "-o", bin/"rclone"
     man1.install "rclone.1"
+    system bin/"rclone", "genautocomplete", "bash", "rclone.bash"
+    system bin/"rclone", "genautocomplete", "zsh", "_rclone"
+    bash_completion.install "rclone.bash" => "rclone"
+    zsh_completion.install "_rclone"
   end
 
   test do
-    system bin/"rclone", "--version"
+    (testpath/"file1.txt").write "Test!"
+    system "#{bin}/rclone", "copy", testpath/"file1.txt", testpath/"dist"
+    assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
   end
 end
