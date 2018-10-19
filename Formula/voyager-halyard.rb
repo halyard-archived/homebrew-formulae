e64a20951cae9657e6b09ddec3725eac021fb85c
Les Aker <me@lesaker.org>
2018-10-20 00:53:18 +0300

    update voyager

--- Formula/voyager-halyard.rb
+++ Formula/voyager-halyard.rb
@@ -2,9 +2,9 @@ class VoyagerHalyard < Formula
   desc "Helper for connecting to AWS accounts"
   homepage "https://github.com/akerl/voyager"
 
-  url "https://github.com/akerl/voyager/releases/download/0.5.0/voyager_darwin"
-  version "0.5.0"
-  sha256 "4f3fc15b7114e8c4589407284b9b28d0bdd7143499f707fe298bb331bbdaa49a"
+  url "https://github.com/akerl/voyager/releases/download/0.6.0/voyager_darwin"
+  version "0.6.0"
+  sha256 "399b46e0445f22507d121edc5815393831e2e0e85658deeedee20a7340f998f9"
 
   head do
     url "https://github.com/akerl/voyager.git"
